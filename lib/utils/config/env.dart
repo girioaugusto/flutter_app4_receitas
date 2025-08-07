8cb2ec0e4006daa8ba22420781a898f0c5253484
Augusto Marconato <girioaugusto@gmail.com>
2025-08-07 19:15:02 -0300

    MVVM, DI e configuração do Supabase-erro corrigido

--- lib/utils/config/env.dart
+++ lib/utils/config/env.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
-  static String get supabaseUrl =>  dotenv.env['SUPABSE_URL'] ?? '';
+  static String get supabaseUrl =>  dotenv.env['SUPABASE_URL'] ?? '';
   static String get supabaseAnnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
 
   static Future<void> init() async {

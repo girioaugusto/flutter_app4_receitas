8ee963d0e7a70782e8b8fdfb698dda1189cab924
girioaugusto <girioaugusto@gmail.com>
2025-08-22 10:08:34 -0300

    Aula 21/08 - test- Completa - Falta signUp e signOut

--- test/data/repositories/auth_repository_test.dart
+++ test/data/repositories/auth_repository_test.dart
@@ -67,9 +67,149 @@ void main() {
         // Assert
         expect(result.isRight, true);
       });
+    
+      test('deve retornar um erro quando o fetchUserProfile falha', () async{
+        // Arrange
+        final mockUser = User(
+        id: 'user-id', 
+        appMetadata: {}, 
+        userMetadata: {}, 
+        aud: 'authenticated', 
+        createdAt: DateTime.now().toIso8601String(),
+        );
+      });
     });
 
-    group('SignInWithPassword', () {});
+    group('SignInWithPassword', () {
+
+      test('deve retornar UserProfile quando login é bem sucedido', () async {
+        // Arrange
+        final mockUser = User(
+        id: 'user-id', 
+        appMetadata: {}, 
+        userMetadata: {}, 
+        aud: 'authenticated', 
+        createdAt: DateTime.now().toIso8601String(),
+        );
+        final mockAuthResponse = AuthResponse(user: mockUser);
+        final mockProfile = {'id': 'user-id', 'username': 'testuser'};
+
+        when(mockAuthService.signInWithPassword(
+            email: 'test@email.com', 
+            password: 'password123'
+          )
+        ).thenAnswer((_) async => Right(mockAuthResponse));
+
+        when (mockAuthService.fetchUserProfile('user-id'),
+        ).thenAnswer((_) async => Right(mockProfile));
+
+        // Act
+        final result = await authRepository.signInWithPassword(
+          email: 'test@email.com', 
+          password: 'password123'
+        );
+
+        // Assert
+
+        expect(result.isRight, true);
+        expect(result.right.username, equals('testuser'));
+
+      });
+
+      test('deve retornar erro quando as credenciais são inválidas', () async {
+        //Arrange
+        when(mockAuthService.signInWithPassword(
+            email: 'test@email.com', 
+            password: 'wrongpassword123',
+          )
+        ).thenAnswer((_) async => 
+          Left(AppError('usuário não cadastrado ou credenciais inválidas'))
+        );
+
+        //Act
+        final result = await authRepository.signInWithPassword(
+          email: 'test@email.com', 
+          password: 'wrongpassword123',
+        );
+
+        //Assert
+        expect(result.isLeft, true);
+        expect(result.left.message, equals('usuário não cadastrado ou credenciais inválidas'));
+      });
+      test('deve retornar erro quando o E-mail não foi confirmado', () async {
+        //Arrange
+        when(mockAuthService.signInWithPassword(
+            email: 'test@email.com', 
+            password: 'password123',
+          )
+        ).thenAnswer((_) async => 
+          Left(AppError('E-mail não confirmado'))
+        );
+
+        //Act
+        final result = await authRepository.signInWithPassword(
+          email: 'test@email.com', 
+          password: 'password123',
+        );
+
+        //Assert
+        expect(result.isLeft, true);
+        expect(result.left.message, equals('E-mail não confirmado'));
+      });
+
+      test('deve retornar erro quando UserProfile falha após login bem sucedido', () async {
+        // Arrange
+        final mockUser = User(
+        id: 'user-id', 
+        appMetadata: {}, 
+        userMetadata: {}, 
+        aud: 'authenticated', 
+        createdAt: DateTime.now().toIso8601String(),
+        );
+        final mockAuthResponse = AuthResponse(user: mockUser);
+
+        when(mockAuthService.signInWithPassword(
+            email: 'test@email.com', 
+            password: 'password123'
+          )
+        ).thenAnswer((_) async => Right(mockAuthResponse));
+
+        when (mockAuthService.fetchUserProfile('user-id'),
+        ).thenAnswer((_) async => Left(AppError('Erro ao carregar profile')));
+
+        // Act
+        final result = await authRepository.signInWithPassword(
+          email: 'test@email.com', 
+          password: 'password123'
+        );
+
+        // Assert
+        expect(result.isLeft, true);
+        expect(result.left.message, equals('Erro ao carregar profile'));
+        verify(mockAuthService.fetchUserProfile('user-id')).called(1);
+      });
+      test('deve retornar erro quando há problema no login', () async {
+        //Arrange
+        when(mockAuthService.signInWithPassword(
+            email: 'test@email.com', 
+            password: 'password123',
+          )
+        ).thenAnswer((_) async => 
+          Left(AppError('Erro ao fazer login'))
+        );
+
+        //Act
+        final result = await authRepository.signInWithPassword(
+          email: 'test@email.com', 
+          password: 'password123',
+        );
+
+        //Assert
+        expect(result.isLeft, true);
+        expect(result.left.message, equals('Erro ao fazer login'));
+      });
+
+    });
 
     group('signUp', () {});
 
